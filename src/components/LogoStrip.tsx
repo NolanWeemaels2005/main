20da7cdb6272e3b8103fd8a657ef72c7ac7e9bde
NolanWeemaels <Nolan.weemaels@student.ehb.be>
2026-04-30 10:37:35 +0200

    tried fixing github pages

--- src/components/LogoStrip.tsx
+++ src/components/LogoStrip.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { featuredProjects } from "../data/projects";
 import { useLanguage } from "../i18n/LanguageContext";
 
@@ -10,9 +11,9 @@ export function LogoStrip() {
         <p>{t("portfolio.clients")}</p>
         <div className="logo-rail">
           {[...featuredProjects, ...featuredProjects].map((project, index) => (
-            <a href={`/portfolio/${project.slug}`} data-cursor="soft" key={`${project.slug}-${index}`}>
+            <Link to={`/portfolio/${project.slug}`} data-cursor="soft" key={`${project.slug}-${index}`}>
               <img src={project.logo} alt={project.titleKey ? t(project.titleKey) : project.title} />
-            </a>
+            </Link>
           ))}
         </div>
       </div>
