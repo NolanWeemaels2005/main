d9fecabf3298de672ad7bc79c9ded6d80f03988d
NolanWeemaels <Nolan.weemaels@student.ehb.be>
2026-04-30 14:45:28 +0200

    fix white page on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "main";
+const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] ?? "FrontendPortfolioWebsite";
 
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? `/${repoName}/` : "/",
